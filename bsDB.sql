46fcef8f3951e940743449289cd7bf71c5c742cd
Brian Eubanks <26339350+BrianEubanks@users.noreply.github.com>
2019-11-26 15:00:58 -0600

    Add files via upload

--- bsDB.sql
+++ bsDB.sql
@@ -2,7 +2,7 @@ drop table message;
 drop table contacts;
 drop table user;
 
-create table user (id, varchar(8), username varchar(16), password varchar(16));
+create table user (id varchar(8), username varchar(16), password varchar(16));
 
 alter table user
 add constraint user_username_pk primary key(username);
